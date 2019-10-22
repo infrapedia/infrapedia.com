618cf9baf7a6c8f2841bf025f916275f1f144de2
manAbl <manuel.22.lamas@gmail.com>
2019-10-22 11:10:48 -0500

    calculation-box style fixings

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="no-overflow">
     <Map @drawing="handleIsDrawing" @title-by-selection="title = $event" />
-    <div v-show="isDrawing" class="calculation-box truncate">
+    <div v-show="isDrawing" class="absolute calculation-box truncate w44 h22 p1 text-center">
       <h4 class="mt2 mb2 font-medium">{{ title }}</h4>
       <div id="calculated-area" class="font-medium fs-regular" />
     </div>
