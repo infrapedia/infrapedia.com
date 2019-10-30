3297a6ebd553e81622435fdbbed13359e31a8312
manAbl <manuel.22.lamas@gmail.com>
2019-10-30 15:02:54 -0500

    dialogs form validations

--- client/src/components/dialogs/BuyDialog.vue
+++ client/src/components/dialogs/BuyDialog.vue
@@ -12,43 +12,43 @@
         Buy Now! - <span class="capitalize">{{ dialogTitle }}</span>
       </h1>
     </header>
-    <el-form class="pr6 pl6">
+    <el-form :model="form" :rules="formRules" class="pr6 pl6" ref="form">
       <el-row :gutter="15">
         <el-col :span="8">
-          <el-form-item label="Company name*">
+          <el-form-item label="Company name" prop="company">
             <el-input v-model="form.company" :class="{ dark }" />
           </el-form-item>
         </el-col>
         <el-col :span="8">
-          <el-form-item label="First Name*">
+          <el-form-item label="First Name" prop="name">
             <el-input v-model="form.name" :class="{ dark }" />
           </el-form-item>
         </el-col>
         <el-col :span="8">
-          <el-form-item label="Last Name*">
+          <el-form-item label="Last Name" prop="lastname">
             <el-input v-model="form.lastname" :class="{ dark }" />
           </el-form-item>
         </el-col>
       </el-row>
       <el-row :gutter="30">
         <el-col :span="12">
-          <el-form-item label="Email*">
+          <el-form-item label="Email" prop="email">
             <el-input type="email" v-model="form.email" :class="{ dark }" />
           </el-form-item>
         </el-col>
         <template v-if="dialogTitle !== 'Other'">
           <el-col :span="12" v-if="dialogTitle !== 'Datacenter'">
-            <el-form-item label="Capacity*">
+            <el-form-item label="Capacity" prop="capacity">
               <el-select
                 class="w-fit-full"
                 :class="{ dark }"
+                filterable
                 v-model="form.capacity"
                 placeholder="Select an option"
               >
                 <el-option
                   v-for="option in capacities"
                   :key="option"
-                  filterable
                   :label="option"
                   :value="option" />
               </el-select>
@@ -69,7 +69,9 @@
       </el-row>
     </el-form>
     <footer class="footer flex justify-content-space-between pr0 pl6">
-      <small>* indicates required field</small>
+      <small class="inline-block mt2">
+        <span class="text-red">*</span> indicates required field
+      </small>
       <div>
         <el-button
           plain
@@ -84,6 +86,7 @@
           type="primary"
           class="w24"
           plain
+          @click="submitForm('form')"
         >
           Buy
         </el-button>
@@ -98,6 +101,7 @@ import { TOGGLE_BUY_DIALOG, BUY_TYPE } from '@/store/actionTypes'
 
 export default {
   data: () => ({
+    capacities: ['1GB', '10GB', '100GB', 'Others'],
     form: {
       company: '',
       email: '',
@@ -107,7 +111,26 @@ export default {
       totalRack: 0,
       issue: ''
     },
-    capacities: ['1GB', '10GB', '100GB', 'Others']
+    formRules: {
+      name: [
+        { required: true, message: 'Please input your first name', trigger: 'blur' },
+        { min: 3, max: 10, message: 'Length should be 3 to 10', trigger: 'blur' }
+      ],
+      lastname: [
+        { required: true, message: 'Please input your last name name', trigger: 'blur' },
+        { min: 3, max: 10, message: 'Length should be 3 to 10', trigger: 'blur' }
+      ],
+      company: [
+        { required: true, message: 'Company name is required', trigger: 'blur' }
+      ],
+      email: [
+        { required: true, message: 'Please input your email', trigger: 'blur' },
+        { type: 'email', required: true, message: 'Please input a valid email', trigger: 'change' }
+      ],
+      capacity: [
+        { required: true, message: 'Please select the desired capacity', trigger: 'change' }
+      ]
+    }
   }),
   computed: {
     ...mapState({
@@ -127,16 +150,28 @@ export default {
     },
     isFormUncomplete() {
       const { name, lastname, capacity, email, company } = this.form
+      let title = this.dialogTitle.toLowerCase()
       let isDisabled = true
-      if (
-        name !== '' &&
-        lastname !== '' &&
-        capacity !== '' &&
-        email !== '' &&
-        company!== ''
-      ) {
-        isDisabled = false
-      }
+
+        if (
+          title !== 'datacenter' &&
+          name !== '' &&
+          lastname !== '' &&
+          capacity !== '' &&
+          email !== '' &&
+          company!== ''
+        ) {
+          isDisabled = false
+        } else if (
+          (title === 'datacenter' || title === 'other') &&
+          company!== '' &&
+          name !== '' &&
+          lastname !== '' &&
+          email !== ''
+        ) {
+          isDisabled = false
+        }
+
       return isDisabled
     },
     customDialogClass() {
@@ -144,8 +179,18 @@ export default {
     }
   },
   methods: {
+    submitForm(formRef) {
+      this.$refs[formRef].validate((valid) => {
+        if (valid) alert('submit!')
+        else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
     closeDialog() {
       this.$store.commit(`${TOGGLE_BUY_DIALOG}`, false)
+      this.$refs.form.resetFields()
       this.form = {
         company: '',
         email: '',
--- client/src/components/dialogs/IssuesDialog.vue
+++ client/src/components/dialogs/IssuesDialog.vue
@@ -12,34 +12,45 @@
         Report your issue
       </h1>
     </header>
-    <el-form class="pr6 pl6">
+    <el-form
+      :model="form"
+      :rules="formRules"
+      class="pr6 pl6"
+      ref="form"
+    >
       <el-row :gutter="15">
         <el-col :span="12">
-          <el-form-item label="First Name*">
+          <el-form-item label="First Name" prop="name">
             <el-input v-model="form.name" :class="{ dark }" />
           </el-form-item>
         </el-col>
         <el-col :span="12">
-          <el-form-item label="Last Name*">
+          <el-form-item label="Last Name" prop="lastname">
             <el-input v-model="form.lastname" :class="{ dark }" />
           </el-form-item>
         </el-col>
       </el-row>
       <el-row :gutter="15">
         <el-col :span="12">
-          <el-form-item label="Email*">
+          <el-form-item label="Email" prop="email">
             <el-input type="email" v-model="form.email" :class="{ dark }" />
           </el-form-item>
         </el-col>
         <el-col :span="12">
-          <el-form-item label="Issue*">
-            <el-input v-model="form.issue" :class="{ dark }" />
+          <el-form-item label="Issue" prop="issue">
+            <el-input
+              type="textarea"
+              :class="{ dark }"
+              v-model="form.issue"
+            />
           </el-form-item>
         </el-col>
       </el-row>
     </el-form>
     <footer class="footer flex justify-content-space-between pr0 pl6">
-      <small class="color-inherit">* indicates required field</small>
+      <small class="inline-block mt2">
+        <span class="text-red">*</span> indicates required field
+      </small>
       <div>
         <el-button
           plain
@@ -54,6 +65,7 @@
           type="primary"
           class="w24"
           plain
+          @click="submitForm('form')"
         >
           Report
         </el-button>
@@ -73,6 +85,27 @@ export default {
       issue: '',
       lastname: ''
     },
+    formRules: {
+      name: [
+        { required: true, message: 'Please input your first name', trigger: 'blur' },
+        { min: 3, max: 10, message: 'Length should be 3 to 10', trigger: 'blur' }
+      ],
+      lastname: [
+        { required: true, message: 'Please input your last name name', trigger: 'blur' },
+        { min: 3, max: 10, message: 'Length should be 3 to 10', trigger: 'blur' }
+      ],
+      company: [
+        { required: true, message: 'Company name is required', trigger: 'blur' }
+      ],
+      email: [
+        { required: true, message: 'Please input your email', trigger: 'blur' },
+        { type: 'email', required: true, message: 'Please input a valid email', trigger: 'change' }
+      ],
+      issue: [
+        { required: true, message: 'Please input the issue', trigger: 'blur' },
+        { min: 10, message: 'Issue length should be at least 10', trigger: 'blur' }
+      ],
+    }
   }),
   computed: {
     isVisible: {
@@ -104,8 +137,18 @@ export default {
     }
   },
   methods: {
+    submitForm(formRef) {
+      this.$refs[formRef].validate((valid) => {
+        if (valid) alert('submit!')
+        else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
     closeDialog() {
       this.$store.commit(`${TOGGLE_ISSUES_DIALOG}`, false)
+      this.$refs.form.resetFields()
       this.form = {
         name: '',
         email: '',
