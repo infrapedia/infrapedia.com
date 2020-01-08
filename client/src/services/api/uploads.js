da9899c82eb0967a7ea8ee64252132ba6d0f4fef
Manuel Blanco <34801587+manAbl@users.noreply.github.com>
2020-01-08 17:26:43 -0500

    url of convertToJson service was wrong, service to uploadKmz file was sending an Array not the file itself

--- client/src/services/api/uploads.js
+++ client/src/services/api/uploads.js
@@ -22,11 +22,12 @@ export const uploadOrgLogo = async ({ logo, user_id }) => {
 export const uploadKmz = async ({ file, user_id }) => {
   url = `${apiConfig.url}/auth/upload/kmz`
   form = new FormData()
-  form.append('file', file)
+  form.append('file', file[0])
   const res = await $axios.post(url, form, {
     withCredentials: true,
     headers: {
       user_id,
+      'Content-Type': 'multipart/form-data',
       Authorization:
         'Bearer ' + window.localStorage.getItem('auth.token-session')
     }
@@ -35,7 +36,7 @@ export const uploadKmz = async ({ file, user_id }) => {
 }
 
 export const kmzToJSON = async ({ link, user_id }) => {
-  url = `${apiConfig.url}/auth/upload/togeojson`
+  url = `${apiConfig.url}/auth/kmz/togeojson`
   form = new FormData()
   form.append('link', link)
   const res = await $axios.post(url, form, {
