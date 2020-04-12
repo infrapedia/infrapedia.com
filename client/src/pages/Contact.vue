f0638402eafbf984295388a416afee3af3a81095
manAbl <manuel.22.lamas@gmail.com>
2020-04-11 21:01:00 -0500

    contact page

--- client/src/pages/Contact.vue
+++ client/src/pages/Contact.vue
@@ -1,41 +1,53 @@
 <template>
-  <div class="main-wrapper">
-    <div class="form-wrapper">
-      <div class="inner-wrapper pt16 pb20 mt16">
-        <h1 class="title inline-block">
+  <div class="main-wrapper mb20">
+    <el-divider class="m0 mb20" />
+    <div class="inner-wrapper">
+      <header class="description_title">
+        <h1 class="title">
           Contact us
         </h1>
-        <el-form :rules="formRules" :model="form" class="p8 form">
+        <p class="text">
+          Feeling like contacting us? Submit your queries here and we will get
+          back to you as soon as possible.
+        </p>
+      </header>
+      <el-card class="form-wrapper p8 round">
+        <header class="mb4">
+          <h2 class="title sm">
+            Send us a Message
+          </h2>
+        </header>
+        <el-form :rules="formRules" :model="form" class="transparent">
           <el-row :gutter="20">
-            <el-col :span="12">
+            <el-col :sm="24" :lg="12">
               <el-form-item label="Name" prop="name" required>
                 <el-input v-model="form.name" />
               </el-form-item>
             </el-col>
-            <el-col :span="12">
+            <el-col :sm="24" :lg="12">
               <el-form-item label="Last name" prop="name" required>
                 <el-input v-model="form.lastname" />
               </el-form-item>
             </el-col>
-            <el-col :span="12">
+            <el-col :sm="24" :lg="12">
               <el-form-item label="Company" prop="company" required>
                 <el-input v-model="form.company" />
               </el-form-item>
             </el-col>
-            <el-col :span="12">
+            <el-col :sm="24" :lg="12">
               <el-form-item label="Email" prop="email" required>
                 <el-input v-model="form.email" />
               </el-form-item>
             </el-col>
-            <el-col :span="24">
+            <el-col :sm="24" :lg="24">
               <el-form-item label="Message" prop="message">
                 <el-input type="textarea" :rows="4" v-model="form.message" />
               </el-form-item>
             </el-col>
-            <el-col :span="24">
+            <el-col :sm="24" :lg="24">
               <el-form-item>
                 <div class="flex mt4 row justify-content-end">
-                  <el-button type="primary">
+                  <el-button round class="w-fit-full" type="primary">
                     Send
                   </el-button>
                 </div>
@@ -43,7 +55,7 @@
             </el-col>
           </el-row>
         </el-form>
-      </div>
+      </el-card>
     </div>
   </div>
 </template>
