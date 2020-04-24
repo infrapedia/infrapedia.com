17752fd330940632a2fee6c69d1cd9073764b260
manAbl <manuel.22.lamas@gmail.com>
2020-04-24 09:54:01 -0500

    print dialog style fixings

---
+++ client/src/config/printDpiOptions.js
@@ -0,0 +1,19 @@
+const printDpiOptions = [
+  {
+    label: '96 dpi - lowest resolution',
+    value: 300,
+    img: 'https://storage.cloud.google.com/infrapediacom/assets/img/96.png'
+  },
+  {
+    label: '150 dpi - regular resolution',
+    value: 150,
+    img: 'https://storage.cloud.google.com/infrapediacom/assets/img/150.png'
+  },
+  {
+    label: '300 dpi - best resolution',
+    value: 96,
+    img: 'https://storage.cloud.google.com/infrapediacom/assets/img/300.png'
+  }
+]
+
+export default printDpiOptions
