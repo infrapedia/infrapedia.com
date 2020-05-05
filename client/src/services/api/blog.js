15ad694f3caf52699ffe8409fe48bf06be89231d
manAbl <manuel.22.lamas@gmail.com>
2020-05-05 03:12:44 -0500

    getBlogPosts service url

--- client/src/services/api/blog.js
+++ client/src/services/api/blog.js
@@ -1,6 +1,8 @@
 import $axios from '../axios'
 
 export default async function getBlogPosts() {
-  const res = await $axios.get('https://www.infrapedia.com/wp-json/wp/v2/posts')
+  const res = await $axios.get(
+    'https://blog.infrapedia.com/wp-json/wp/v2/posts'
+  )
   return res
 }
