1720253335c5a58d2ba22baece219f3f9c5c9fbf
manAbl <manuel.22.lamas@gmail.com>
2020-05-10 23:00:36 -0500

    prod files

---
+++ client/dist/js/chunk-2d0c2187.49bced95.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c2187"],{"499e":function(n,c,t){"use strict";t.r(c);var u=t("7c8d");function e(n){var c=[1/0,1/0,-1/0,-1/0];return Object(u["a"])(n,(function(n){c[0]>n[0]&&(c[0]=n[0]),c[1]>n[1]&&(c[1]=n[1]),c[2]<n[0]&&(c[2]=n[0]),c[3]<n[1]&&(c[3]=n[1])})),c}c["default"]=e}}]);
+//# sourceMappingURL=chunk-2d0c2187.49bced95.js.map
