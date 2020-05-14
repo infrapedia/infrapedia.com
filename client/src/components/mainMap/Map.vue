0756f9028f083f1c8ada030b7bc0fc13c23ad91e
manAbl <manuel.22.lamas@gmail.com>
2020-05-13 19:29:03 -0500

    shareLinks now open a new window except the regular share view

--- client/src/components/mainMap/Map.vue
+++ client/src/components/mainMap/Map.vue
@@ -779,15 +779,21 @@ export default {
       if (hasToEase) {
         await this.handleFocusOnEasePoints()
       } else if (focus && bounds && bounds.length) {
-        await map.fitBounds(bounds, {
-          padding: isMobile ? 10 : 35,
-          maxZoom: 16.5,
-          animate: true,
-          speed: 1.75,
-          pan: {
-            duration: 25
-          }
-        })
+        try {
+          await map.fitBounds(bounds, {
+            padding: isMobile ? 10 : 35,
+            maxZoom: 16.5,
+            animate: true,
+            speed: 1.75,
+            pan: {
+              duration: 25
+            }
+          })
+        } catch {
+          // Ignore
+          // Even thought that it throws an error
+          // The bounds are been set correctly
+        }
       }
     },
     /**
@@ -797,10 +803,16 @@ export default {
       const { map, focus, bounds, hasToEase } = this
       if (hasToEase) await this.handleFocusOnEasePoints()
       else if (focus && bounds && bounds.length) {
-        await map.fitBounds(bounds, {
-          padding: 20,
-          zoom: 4
-        })
+        try {
+          await map.fitBounds(bounds, {
+            padding: 20,
+            zoom: 4
+          })
+        } catch {
+          // Ignore
+          // Even thought that it throws an error
+          // The bounds are been set correctly
+        }
       }
 
       await this.handleCablesSelection(true, [{ properties: { _id: id } }])
@@ -813,30 +825,41 @@ export default {
 
       await this.handleFacilitySelection({ id, type })
       if (focus && bounds && bounds.length) {
-        map.fitBounds(bounds, {
-          pan: { duration: 25 },
-          animate: true,
-          padding: 20,
-          speed: 1.1,
-          zoom: 18,
-          pitch: 45
-        })
+        try {
+          map.fitBounds(bounds, {
+            pan: { duration: 25 },
+            animate: true,
+            padding: 20,
+            speed: 1.1,
+            zoom: 18,
+            pitch: 45
+          })
+        } catch {
+          // Ignore
+          // Even thought that it throws an error
+          // The bounds are been set correctly
+        }
       }
     },
     async handleClsFocus({ id, type }) {
       const { map, focus, bounds } = this
 
       await this.handleClsSelection({ id, type })
-
       if (focus && bounds && bounds.length) {
-        map.fitBounds(bounds, {
-          pan: { duration: 25 },
-          animate: true,
-          padding: 20,
-          speed: 1.1,
-          zoom: 12,
-          pitch: 45
-        })
+        try {
+          map.fitBounds([bounds, bounds], {
+            pan: { duration: 25 },
+            animate: true,
+            padding: 20,
+            speed: 1.1,
+            zoom: 12,
+            pitch: 45
+          })
+        } catch {
+          // Ignore
+          // Even thought that it throws an error
+          // The bounds are been set correctly
+        }
       }
     },
     async handleIxpsFocus(data) {
@@ -844,14 +867,20 @@ export default {
 
       await this.handleIxpsSelection(data)
       if (focus && bounds && bounds.length) {
-        map.fitBounds(bounds, {
-          pan: { duration: 25 },
-          animate: true,
-          padding: 20,
-          speed: 1.1,
-          zoom: 18,
-          pitch: 45
-        })
+        try {
+          map.fitBounds([bounds, bounds], {
+            pan: { duration: 25 },
+            animate: true,
+            padding: 20,
+            speed: 1.1,
+            zoom: 18,
+            pitch: 45
+          })
+        } catch {
+          // Ignore
+          // Even thought that it throws an error
+          // The bounds are been set correctly
+        }
       }
     },
     handleFocusOnEasePoints: debounce(async function() {
