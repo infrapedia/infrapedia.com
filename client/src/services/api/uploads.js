cd34537ee472c27263a9510d690bdc8b3be6c068
manAbl <manuel.22.lamas@gmail.com>
2020-05-27 18:24:45 -0500

    element id was missing in editElement service

--- client/src/services/api/uploads.js
+++ client/src/services/api/uploads.js
@@ -61,11 +61,18 @@ export const kmzPointsToJSON = async ({ link, user_id }) => {
   return res
 }
 
-export const editElemnt = async ({ t, file, information, user_id }) => {
+export const editElemnt = async ({
+  t,
+  element,
+  file,
+  information,
+  user_id
+}) => {
   url = `${apiConfig.url}/auth/elements/foredit`
   form = new FormData()
 
   form.append('t', t)
+  form.append('element', element)
   form.append('file', file)
   form.append('information', information)
 
