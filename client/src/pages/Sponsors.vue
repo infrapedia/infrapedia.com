efb09e026fc39a6f03e92338f26acfac54fb8ae4
manAbl <manuel.22.lamas@gmail.com>
2020-06-08 23:13:05 -0500

    sponsors page price fixings

--- client/src/pages/Sponsors.vue
+++ client/src/pages/Sponsors.vue
@@ -100,143 +100,14 @@
 </template>
 
 <script>
+import { sponsorsFeatures, ads, pricing } from '../config/sponsorships'
+
 export default {
   name: 'SponsorsPage',
   data: () => ({
-    sponsorsFeatures: [
-      {
-        feature: 'Support Development',
-        partner: 'Yes',
-        premium: 'Yes',
-        exclusive: 'Yes'
-      },
-      {
-        feature: 'Receive Buy Capacity Requests',
-        partner: 'No',
-        premium: 'Yes',
-        exclusive: 'Yes'
-      },
-      {
-        feature: 'Highlight facilities on map',
-        partner: 'No',
-        premium: 'Yes',
-        exclusive: 'Yes'
-      },
-      {
-        feature: 'Banner on map',
-        partner: 'No',
-        premium: 'No',
-        exclusive: 'Yes - top banner with logo, brand name and short copy'
-      },
-      {
-        feature: 'Logo on Infrapedia.com',
-        partner: 'No',
-        premium: 'No',
-        exclusive: 'Yes'
-      },
-      {
-        feature:
-          'Logo and mention on Social Media (Facebook, Linkedin, Instagram, Twitter)',
-        partner: 'Yes - one mention with logo and tag',
-        premium: 'Yes - two mentions with logo and tag',
-        exclusive: 'Yes - weekly mentions with logo, tag and link'
-      },
-      {
-        feature: 'Logo on "Partners" page on Infrapedia.com',
-        partner: 'Yes',
-        premium: 'Yes',
-        exclusive: 'Yes'
-      },
-      {
-        feature: 'Logo on "Premium Partners" page on live.infrapedia.com',
-        partner: 'No',
-        premium: 'Yes',
-        exclusive: 'Yes, first/top place'
-      },
-      {
-        feature: 'Logo in Newsletters Sponsor Footer Area',
-        partner: 'No',
-        premium: 'Yes',
-        exclusive: 'Yes - on every newsletter'
-      },
-      {
-        feature: 'Logo in Menu Bar of Interactive Map',
-        partner: 'No',
-        premium: 'No',
-        exclusive: 'Yes'
-      },
-      {
-        feature: 'Sponsored Youtube Video',
-        partner: 'No',
-        premium: 'No',
-        exclusive: 'Yes'
-      },
-      {
-        feature: 'Sponsored Blog Post',
-        partner: 'No',
-        premium: 'No',
-        exclusive: 'Yes'
-      },
-      {
-        feature:
-          'Logo in our Conference Roadshows, Presentations, and Webinars',
-        partner: 'No',
-        premium: 'Yes (webinars)',
-        exclusive: 'Yes'
-      },
-      {
-        feature: 'Share your data for free',
-        partner: 'Yes',
-        premium: 'Yes',
-        exclusive: 'Yes - include your logo'
-      }
-    ],
-    pricing: [
-      {
-        recurrence: 'Monthly',
-        partner: 'N/A',
-        premium: '$750',
-        exclusive: 'Contact us for details'
-      },
-      {
-        recurrence: 'Yearly',
-        partner: '$2,500',
-        premium: '$7,500',
-        exclusive: 'Contact us for details'
-      },
-      {
-        recurrence: 'Share your Data Discount (yearly plans only)',
-        partner: '$2,500',
-        premium: '$2,500',
-        exclusive: 'Contact us for details'
-      },
-      {
-        recurrence: 'You pay',
-        partner: '$0',
-        premium: '$5,500',
-        exclusive: 'Contact us for details'
-      }
-    ],
-    ads: [
-      {
-        ad: 'Top Side Banner Ad Infrapedia.com',
-        max: 1,
-        monthly: '$500',
-        yearly: '$5,000'
-      },
-      {
-        ad: 'Side Banner Ad Infrapedia Blog',
-        max: 2,
-        monthly: '$350',
-        yearly: '$3,500'
-      },
-      {
-        ad: 'Ad on live map',
-        max: 2,
-        monthly: '$600',
-        yearly: '$6,000'
-      }
-    ]
+    sponsorsFeatures,
+    pricing,
+    ads
   }),
   head: {
     title:
