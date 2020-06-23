97392baacf0ea702f414b1afc98015da7c063da6
manAbl <manuel.22.lamas@gmail.com>
2020-06-22 19:27:39 -0500

    landing page style changes

--- client/src/components/homepage/HomePage.vue
+++ client/src/components/homepage/HomePage.vue
@@ -3,15 +3,15 @@
     <div class="hero-wrapper relative">
       <div
         class="absolute z-index20 cubic-transition overlay-inner w-fit-full h-fit-full flex column nowrap justify-content-center align-items-center"
+        :class="{ 'visible-from-start': isMobile }"
       >
         <div class="inner-wrapper-text flex justify-content-center row wrap">
-          <h2 class="title white mb12">
-            Create your account today and begin to explore
+          <h1 class="title white mb12">
+            Create your free account today and begin to explore.
             <small class="block">
-              We have a variety of features that make it easy for you to manage
-              your assets
+              Take advantage of all the features Infrapedia has for you.
             </small>
-          </h2>
+          </h1>
           <el-button
             class="mr4 w42"
             plain
@@ -84,11 +84,11 @@
         </span>
       </router-link>
     </div>
-    <div class="carousel-section">
+    <div class="carousel-section pb20">
       <div class="carousel-wrapper p4">
         <el-divider></el-divider>
         <div class="mt20">
-          <h3 class="title">Blog</h3>
+          <h2 class="title">Blog</h2>
           <el-carousel
             :interval="7000"
             :height="blogHeight"
@@ -114,46 +114,31 @@
           </el-carousel>
         </div>
       </div>
-      <div class="carousel-wrapper p4">
+      <div class="section-wrapper p4">
         <el-divider></el-divider>
         <div class="mt20">
-          <h3 class="title">Our Partners</h3>
-          <el-carousel
-            :interval="6000"
-            height="40vh"
-            indicator-position="outside"
-          >
-            <el-carousel-item v-for="(img, i) in premium" :key="i">
-              <div class="flex row nowrap justify-content-center">
-                <el-card shadow="never" class="no-border w40">
-                  <el-image
-                    :src="img.logo"
-                    fit="scale-down"
-                    class="img-sponsor w40 h80 no-selectable"
-                  />
-                </el-card>
-              </div>
-            </el-carousel-item>
-          </el-carousel>
+          <h2 class="title mb20">Our Partners</h2>
+          <div class="boxes-wrapper">
+            <div
+              class="el-card box p4"
+              :class="{ 'no-gray-filter': isMobile }"
+              v-for="(partner, i) in premium"
+              :key="i"
+            >
+              <el-image fit="contain" :src="partner.logo" />
+            </div>
+          </div>
         </div>
       </div>
-      <div class="carousel-wrapper p4">
-        <h3 class="title">Trusted by</h3>
-        <el-carousel
-          :interval="5000"
-          height="40vh"
-          indicator-position="outside"
-        >
-          <el-carousel-item v-for="(img, i) in trustedBy" :key="i">
-            <div class="flex row nowrap justify-content-center">
-              <el-image
-                :src="img.logo"
-                fit="scale-down"
-                class="img-sponsor w40 h80 no-selectable"
-              />
-            </div>
-          </el-carousel-item>
-        </el-carousel>
+      <div class="section-wrapper p4">
+        <el-divider></el-divider>
+        <h2 class="title mb20">Trusted by</h2>
+        <div class="boxes-wrapper">
+          <div class="box p4 el-card" v-for="(partner, i) in 12" :key="i">
+            <!-- <el-image fit="contain" lazy :src="partner" /> -->
+            {{ partner }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -171,33 +156,35 @@ export default {
     'i-map': Map
   },
   data: () => ({
-    texts: {
-      withIcon: [
-        {
-          title: 'Versatile',
-          icon: ['fas', 'people-arrows'],
-          text:
-            'This is an extremely useful feature, especially during times when attending conferences and in-person meetings is a challenge.'
-        },
-        {
-          title: 'Discover',
-          icon: ['fas', 'globe-americas'],
-          text:
-            'We bring more visibility to all routes including submarine and terrestrial networks in detail, plus other critical structures, including data centers.'
-        },
-        {
-          title: 'Connect',
-          icon: ['fas', 'headset'],
-          text:
-            'The technical details we provide will help your team build more resilient networks. Our team is a direct funnel to connect you with sales teams to acquire capacity without commissions.'
-        }
-      ]
-    },
     trustedBy: [],
     blogPosts: [],
     loadingVotes: false
   }),
   computed: {
+    texts() {
+      return {
+        withIcon: [
+          {
+            title: 'Versatile',
+            icon: ['fas', 'people-arrows'],
+            text:
+              'This is an extremely useful feature, especially during times when attending conferences and in-person meetings is a challenge.'
+          },
+          {
+            title: 'Discover',
+            icon: ['fas', 'globe-americas'],
+            text:
+              'We bring more visibility to all routes including submarine and terrestrial networks in detail, plus other critical structures, including data centers.'
+          },
+          {
+            title: 'Connect',
+            icon: ['fas', 'headset'],
+            text:
+              'The technical details we provide will help your team build more resilient networks. Our team is a direct funnel to connect you with sales teams to acquire capacity without commissions.'
+          }
+        ]
+      }
+    },
     formatDate() {
       return function(date) {
         return formatDate(date)
@@ -207,7 +194,10 @@ export default {
       return this.$store.state.premium
     },
     blogHeight() {
-      return window.innerWidth <= 425 ? '40vh' : '28vh'
+      return this.isMobile ? '40vh' : '28vh'
+    },
+    isMobile() {
+      return window.innerWidth <= 425
     }
   },
   async mounted() {
