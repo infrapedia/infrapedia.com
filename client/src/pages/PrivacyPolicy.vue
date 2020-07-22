0e80aad99376d8de64c12c58cd8eff1b677bdb1b
manAbl <manuel.22.lamas@gmail.com>
2020-07-22 12:29:16 -0500

    terms and privacy policy pages updated

--- client/src/pages/PrivacyPolicy.vue
+++ client/src/pages/PrivacyPolicy.vue
@@ -7,110 +7,91 @@
 
       <p>
         Protecting your private information is our priority. This Statement of
-        Privacy applies to
-        <a href="https://www.infrapedia.com">https://www.infrapedia.com</a>
-        and Kapany Networks, Inc. and governs data collection and usage. For the
-        purposes of this Privacy Policy, unless otherwise noted, all references
-        to Kapany Networks, Inc. include
-        <a href="https://www.infrapedia.com">https://www.infrapedia.com</a>,
-        Network Atlas, Kapany Networks, Inc.,
-        <a href="https://www.kapany.net">www.kapany.net</a> and
-        <a href="https://dev.networkatlas.org">dev.networkatlas.org</a>. The
-        Network Atlas website is a Network Atlas Service site. By using the
-        Network Atlas website, you consent to the data practices described in
-        this statement.
+        Privacy applies to https://www.infrapedia.com and Kapany Networks, Inc.
+        and governs data collection and usage. For the purposes of this Privacy
+        Policy, unless otherwise noted, all references to Kapany Networks, Inc.
+        include https://www.infrapedia.com, Infrapedia, Kapany Networks, Inc.,
+        www.kapany.net and dev.infrapedia.com. The Infrapedia website is an
+        Infrapedia Service site. By using the Infrapedia website, you consent to
+        the data practices described in this statement.
       </p>
 
       <h3>Collection of your Personal Information</h3>
-      In order to better provide you with products and services offered on our
-      Site, Network Atlas may collect personally identifiable information, such
-      as your:
-      <ul class="ml3">
-        <li>First and Last Name</li>
+      <p>
+        In order to better provide you with products and services offered on our
+        Site, Infrapedia may collect personally identifiable information, such
+        as your:
+      </p>
+      <ol class="ml1">
+        <li class="mb4">First and Last Name</li>
         <li>E-mail Address</li>
-      </ul>
+      </ol>
       <p>
         We do not collect any personal information about you unless you
         voluntarily provide it to us. However, you may be required to provide
         certain personal information to us when you elect to use certain
-        products or services available on the Site. These may include:
+        products or services available on the Site. These may include: (a)
+        registering for an account on our Site; (b) entering a sweepstakes or
+        contest sponsored by us or one of our partners; (c) signing up for
+        special offers from selected third parties; (d) sending us an email
+        message; (e) submitting your credit card or other payment information
+        when ordering and purchasing products and services on our Site. To wit,
+        we will use your information for, but not limited to, communicating with
+        you in relation to services and/or products you have requested from us.
+        We also may gather additional personal or non-personal information in
+        the future.
       </p>
-      <ol class="capitalize">
-        <li class="mb4">
-          registering for an account on our Site
-        </li>
 
-        <li class="mb4">
-          entering a sweepstakes or contest sponsored by us or one of our
-          partners
-        </li>
-
-        <li class="mb4">
-          signing up for special offers from selected third parties
-        </li>
-        <li class="mb4">sending us an email message</li>
-        <li class="mb4">
-          submitting your credit card or other payment information when ordering
-          and purchasing products and services on our Site
-        </li>
-      </ol>
-
-      <p>
-        To wit, we will use your information for, but not limited to,
-        communicating with you in relation to services and/or products you have
-        requested from us. We also may gather additional personal or
-        non-personal information in the future.
-      </p>
       <h3>
         Use of your Personal Information
       </h3>
       <p>
-        Network Atlas collects and uses your personal information to operate its
+        Infrapedia collects and uses your personal information to operate its
         website(s) and deliver the services you have requested.
       </p>
       <p>
-        Network Atlas may also use your personally identifiable information to
-        inform you of other products or services available from Network Atlas
-        and its affiliates.
+        Infrapedia may also use your personally identifiable information to
+        inform you of other products or services available from Infrapedia and
+        its affiliates.
       </p>
       <h3>
         Sharing Information with Third Parties
       </h3>
       <p>
-        Network Atlas does not sell, rent or lease its customer lists to third
+        Infrapedia does not sell, rent or lease its customer lists to third
         parties.
       </p>
       <p>
-        Network Atlas may, from time to time, contact you on behalf of external
+        Infrapedia may, from time to time, contact you on behalf of external
         business partners about a particular offering that may be of interest to
         you. In those cases, your unique personally identifiable information
         (e-mail, name, address, telephone number) is not transferred to the
-        third party. Network Atlas may share data with trusted partners to help
+        third party. Infrapedia may share data with trusted partners to help
         perform statistical analysis, send you email or postal mail, provide
         customer support, or arrange for deliveries. All such third parties are
         prohibited from using your personal information except to provide these
-        services to Network Atlas, and they are required to maintain the
+        services to Infrapedia, and they are required to maintain the
         confidentiality of your information.
       </p>
       <p>
-        Network Atlas may disclose your personal information, without notice, if
+        Infrapedia may disclose your personal information, without notice, if
         required to do so by law or in the good faith belief that such action is
         necessary to: (a) conform to the edicts of the law or comply with legal
-        process served on Network Atlas or the site; (b) protect and defend the
-        rights or property of Network Atlas; and/or (c) act under exigent
-        circumstances to protect the personal safety of users of Network Atlas,
-        or the public.
+        process served on Infrapedia or the site; (b) protect and defend the
+        rights or property of Infrapedia; and/or (c) act under exigent
+        circumstances to protect the personal safety of users of Infrapedia, or
+        the public
       </p>
       <h3>
         Automatically Collected Information
       </h3>
       <p>
         Information about your computer hardware and software may be
-        automatically collected by Network Atlas. This information can include:
+        automatically collected by Infrapedia. This information can include:
         your IP address, browser type, domain names, access times and referring
         website addresses. This information is used for the operation of the
         service, to maintain quality of the service, and to provide general
-        statistics regarding use of the Network Atlas website.
+        statistics regarding use of the Infrapedia website.
       </p>
       <h3>
         Links
@@ -124,13 +105,13 @@
       </p>
       <h3>Security of your Personal Information</h3>
       <p>
-        Network Atlas secures your personal information from unauthorized
-        access, use, or disclosure. Network Atlas uses the following methods for
-        this purpose:
+        Infrapedia secures your personal information from unauthorized access,
+        use, or disclosure. Infrapedia uses the following methods for this
+        purpose:
       </p>
-      <ul>
+      <ul class="ml1">
         <li>
-          SSL Protocol
+          - SSL Protocol
         </li>
       </ul>
       <p>
@@ -152,7 +133,7 @@
 
       <h3>Children Under Thirteen</h3>
       <p>
-        Network Atlas does not knowingly collect personally identifiable
+        Infrapedia does not knowingly collect personally identifiable
         information from children under the age of thirteen. If you are under
         the age of thirteen, you must ask your parent or guardian for permission
         to use this website.
@@ -160,13 +141,13 @@
 
       <h3>E-mail Communications</h3>
       <p>
-        From time to time, Network Atlas may contact you via email for the
-        purpose of providing announcements, promotional offers, alerts,
-        confirmations, surveys, and/or other general communication.
+        From time to time, Infrapedia may contact you via email for the purpose
+        of providing announcements, promotional offers, alerts, confirmations,
+        surveys, and/or other general communication.
       </p>
       <p>
         If you would like to stop receiving marketing or promotional
-        communications via email from Network Atlas, you may opt out of such
+        communications via email from Infrapedia, you may opt out of such
         communications by emailing
         <a href="mailto:contact@kapany.net" target="_blank"
           >contact@kapany.net</a
@@ -181,22 +162,21 @@
 
       <h3>Changes to this Statement</h3>
       <p>
-        Network Atlas reserves the right to change this Privacy Policy from time
-        to time. We will notify you about significant changes in the way we
-        treat personal information by sending a notice to the primary email
-        address specified in your account, by placing a prominent notice on our
-        site, and/or by updating any privacy information on this page. Your
-        continued use of the Site and/or Services available through this Site
-        after such modifications will constitute your: (a) acknowledgment of the
-        modified Privacy Policy; and (b) agreement to abide and be bound by that
-        Policy.
+        Infrapedia reserves the right to change this Privacy Policy from time to
+        time. We will notify you about significant changes in the way we treat
+        personal information by sending a notice to the primary email address
+        specified in your account, by placing a prominent notice on our site,
+        and/or by updating any privacy information on this page. Your continued
+        use of the Site and/or Services available through this Site after such
+        modifications will constitute your: (a) acknowledgment of the modified
+        Privacy Policy; and (b) agreement to abide and be bound by that Policy.
       </p>
 
       <h3>Contact Information</h3>
       <p>
-        Network Atlas welcomes your questions or comments regarding this
-        Statement of Privacy. If you believe that Network Atlas has not adhered
-        to this Statement, please contact Network Atlas at:
+        Infrapedia welcomes your questions or comments regarding this Statement
+        of Privacy. If you believe that Infrapedia has not adhered to this
+        Statement, please contact Infrapedia at:
       </p>
 
       <p>
