6fe9c8a9d889191f7a2fb06b5df69d8ae39655d1
manAbl <manuel.22.lamas@gmail.com>
2020-07-22 13:08:59 -0500

    sponsors, about us page changes

--- client/src/pages/About.vue
+++ client/src/pages/About.vue
@@ -1,59 +1,70 @@
 <template>
   <div
-    class="main-wrapper flex column wrap justify-content-center no-overflow-x"
+    class="main-wrapper flex column wrap justify-content-center no-overflow-x pt20"
   >
-    <div class="banner-wrapper mb8">
-      <el-image
-        lazy
-        fit="cover"
-        src="https://cdn1.infrapedia.com/bgs/b3.jpg"
-        class="banner-img"
-      />
-    </div>
-    <div class="gallery-wrapper">
-      <header class="w-fit-full text-center">
-        <h1 class="title mb8">
-          About us
-        </h1>
-      </header>
-      <p class="content">
-        Infrapedia is a crowd-sourced near real-time map of the global internet
-        infrastructure detailing the world’s submarine and terrestrials
-        networks. You can add networks to the map and update listings with more
-        accurate information, track outages and explore possible areas of
-        expansion. We're a community that seeks to give engineers an operational
-        global view of the internet infrastructure. We connect network
-        procurement professionals directly with infrastructure owners. We have
-        more than 3,500 datacenters, 15,000 networks, and hundreds of submarine
-        and terrestrial network providers.
-      </p>
+    <div class="inner-wrapper">
+      <template>
+        <header>
+          <h1 class="title mb8">
+            About us
+          </h1>
+        </header>
+        <p class="content">
+          Our focus is to show the layer 1 structure of the global Internet and
+          develop a platform that allows community members to engage directly
+          with infrastructure and datacenter providers to swap resources, see
+          special deals and more. No commissions, no bias.
+        </p>
+        <p class="content">
+          We scour the Internet and maintain close relationships with key market
+          players, industry specialists and Internet resources to find
+          infrastructure assets around the world and update our databases on a
+          daily basis with subsea cable and data center statuses, outages, and
+          social media posts.
+        </p>
+        <p class="content">
+          Infrapedia is the only available public tool for engineers,
+          decision-makers and infrastructure experts that gives an operational
+          global view of the internet infrastructure to support identifying
+          possible areas of expansion, new opportunities and potential
+          collaborations with other community members.
+        </p>
+        <p class="content">
+          We do not stand alone. With the help of our
+          <router-link to="/advisory-board">Board of Advisors</router-link>
+          consisting of industry specialists, we are developing awareness of our
+          community, reaching out to other carriers, service providers and
+          Internet business such as Google, Microsoft and Facebook. And with the
+          help from our Partners and
+          <router-link to="/sponsors">Sponsors</router-link>
+          Infrapedia is able to keep providing the world’s most realistic view
+          of the internet infrastructure for free by assuring the long term
+          sustainability of the platform and community.
+        </p>
+      </template>
 
-      <div class="team-wrapper mb20">
+      <div class="team-wrapper w-fit-full mb20">
         <h2 class="title">
           Our team
         </h2>
-        <div class="boxes-wrapper mt20 mb20">
-          <div class="box text-center" v-for="(p, i) of teamMembers" :key="i">
+        <div class="boxes-wrapper flex row wrap mt20 mb20">
+          <div
+            class="box text-center mb12"
+            v-for="(p, i) of teamMembers"
+            :key="i"
+          >
             <div class="image-wrapper">
-              <el-image class="img" lazy :src="p.photo" />
+              <el-image class="img circle" lazy :src="p.photo" />
             </div>
             <div class="text-wrapper">
-              <strong class="inline-block mt4">
+              <span class="font-bold mb1 inline-block">
                 {{ p.name }}
-              </strong>
+              </span>
               <br />
-              <p class="title sm diff">{{ p.position }}</p>
+              <span class="title sm diff">{{ p.position }}</span>
             </div>
           </div>
         </div>
-        <p class="content">
-          We are focused on network and data center infrastructures. We scour
-          the Internet and maintain close relationships with key market players,
-          industry specialists, and Internet resources to find infrastructure
-          assets around the world and update our databases on a daily basis with
-          subsea cable and data center statuses, outages, and social media
-          posts.
-        </p>
       </div>
     </div>
   </div>
