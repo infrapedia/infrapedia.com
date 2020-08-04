b4f127c87f3dd8863f92a235db9fe906cd5b9153
manAbl <manuel.22.lamas@gmail.com>
2020-08-04 00:41:54 -0500

    removed debugger statement

--- client/src/pages/profile/section/create.vue
+++ client/src/pages/profile/section/create.vue
@@ -728,7 +728,6 @@ export default {
       if (res.t != 'error') return this.handleReturningRoute(this.creationType)
     },
     async createCable() {
-      debugger
       this.isSendingData = true
       const {
         t,
