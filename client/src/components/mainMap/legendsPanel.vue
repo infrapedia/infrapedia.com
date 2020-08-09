68f2b8bce7d7d70798f56865017b0f7f9d112a91
manAbl <manuel.22.lamas@gmail.com>
2020-08-09 01:23:37 -0500

    legends panel style fixings

--- client/src/components/mainMap/legendsPanel.vue
+++ client/src/components/mainMap/legendsPanel.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="legends-wrapper flex column bottom-shadow p4" :class="{ dark }">
-    <header class="pt2 pb2">
+  <div class="legends-wrapper flex column bottom-shadow" :class="{ dark }">
+    <header class="pt2 pb2 pr4 pl4">
       Legends.
     </header>
     <el-divider :class="{ dark }" class="mt1 mb1 w-fit-full" />
     <div
-      class="box p1 inline-flex align-items-center seamless-hoverbg"
+      class="box p1 mb1 inline-flex align-items-center seamless-hoverbg pr4 pl4"
       :class="{ light: !dark, dark }"
       v-for="b in legends"
       :key="b.label"
     >
       <span
-        class="color inline-block w4 h4 mr1"
+        class="color inline-block w4 h4 mr1 circle"
         :style="`background-color:${b.color}`"
       />
       <span class="capitalize">
