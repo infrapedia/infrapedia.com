4c2b92131975dec4eaf12ec8a95a76ec045cf490
manAbl <manuel.22.lamas@gmail.com>
2020-08-16 12:34:08 -0500

    catching ixps edit mode

--- client/src/pages/profile/section/create.vue
+++ client/src/pages/profile/section/create.vue
@@ -473,37 +473,41 @@ export default {
       }
     },
     async getElementOnEdit(_id) {
-      this.loading = true
-      this.mode = 'edit'
-      let currentElement = {}
+      try {
+        this.loading = true
+        this.mode = 'edit'
+        let currentElement = {}
 
-      switch (this.creationType) {
-        case 'cls':
-          currentElement = await this.viewCurrentCLS(_id)
-          if (!currentElement.country || currentElement.country == 'null') {
-            currentElement.country = ''
-          }
-          break
-        case 'facilities':
-          currentElement = await this.viewCurrentFacility(_id)
-          break
-        case 'ixps':
-          currentElement = await this.viewCurrentIXP(_id)
-          currentElement.owners
-            ? currentElement.owners
-            : (currentElement.owners = [])
-          break
-        default:
-          currentElement = await this.viewCurrentCable(_id)
-          if (this.creationType == 'subsea' && !currentElement.litCapacity) {
-            currentElement.litCapacity = []
-          }
-          break
+        switch (this.creationType) {
+          case 'cls':
+            currentElement = await this.viewCurrentCLS(_id)
+            if (!currentElement.country || currentElement.country == 'null') {
+              currentElement.country = ''
+            }
+            break
+          case 'facilities':
+            currentElement = await this.viewCurrentFacility(_id)
+            break
+          case 'ixps':
+            currentElement = await this.viewCurrentIXP(_id)
+            currentElement.owners
+              ? currentElement.owners
+              : (currentElement.owners = [])
+            break
+          default:
+            currentElement = await this.viewCurrentCable(_id)
+            if (this.creationType == 'subsea' && !currentElement.litCapacity) {
+              currentElement.litCapacity = []
+            }
+            break
+        }
+
+        this.form = { ...currentElement }
+        await this.handleEditModeSettings(currentElement)
+        this.loading = false
+      } catch (err) {
+        console.error(err)
       }
-
-      this.form = { ...currentElement }
-      await this.handleEditModeSettings(currentElement)
-      this.loading = false
     },
     async handleEditModeSettings(data) {
       switch (this.creationType) {
@@ -527,17 +531,19 @@ export default {
       // when it's a point feature
       {
         features =
-          data.geom.type == 'Point'
+          data.geom.type == 'Point' || data.geom.type == 'Feature'
             ? [
                 {
                   type: 'Feature',
                   properties: data.geom.properties
-                    ? { ...data.geom.properties }
+                    ? data.geom.properties
                     : { name: '' },
-                  geometry: {
-                    type: data.geom.type,
-                    coordinates: data.geom.coordinates
-                  }
+                  geometry: data.geom.geometry
+                    ? data.geom.geometry
+                    : {
+                        type: data.geom.type,
+                        coordinates: data.geom.coordinates
+                      }
                 }
               ]
             : [...data.geom.features]
@@ -596,6 +602,8 @@ export default {
       }
       if (!this.form.geom || this.form.geom == 'undefined') {
         this.form.geom = this.featuresList
+      } else if (data.geom && !Array.isArray(data.geom)) {
+        this.form.geom = [data.geom]
       }
 
       if (data.owners && Array.isArray(data.owners)) {
