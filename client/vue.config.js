4650b48be76161b78a975e9178a6d032b68a5d86
manAbl <manuel.22.lamas@gmail.com>
2020-09-14 14:39:50 -0500

    removed plugin causing errors on console

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,56 +1,56 @@
-const fs = require('fs')
-const path = require('path')
-const PrerenderSpaPlugin = require('prerender-spa-plugin')
+// const fs = require('fs')
+// const path = require('path')
+// const PrerenderSpaPlugin = require('prerender-spa-plugin')
 const WebpackBar = require('webpackbar')
 const CompressionPlugin = require('compression-webpack-plugin')
-const gzip = require('@gfx/zopfli').gzip
+// const gzip = require('@gfx/zopfli').gzip
 
-function getRoutes() {
-  let arr = []
-  if (process.env.NODE_ENV == 'production') {
-    fs.readdir('./src/pages', (err, files) => {
-      arr = Array.from(files)
-    })
-  }
-  return arr
-}
+// function getRoutes() {
+//   let arr = []
+//   if (process.env.NODE_ENV == 'production') {
+//     fs.readdir('./src/pages', (err, files) => {
+//       arr = Array.from(files)
+//     })
+//   }
+//   return arr
+// }
 
-const productionPlugins = [
-  new PrerenderSpaPlugin({
-    staticDir: path.join(__dirname, 'dist'),
-    routes: getRoutes(),
-    postProcess(renderedRoute) {
-      renderedRoute.html = renderedRoute.html
-        .replace(/<script (.*?)>/g, '<script $1 defer>')
-        .replace('id="app"', 'id="app" data-server-rendered="true"')
-      return renderedRoute
-    },
-    renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
-      // We need to inject a value so we're able to
-      // detect if the page is currently pre-rendered.
-      inject: {},
-      // Our view component is rendered after the API
-      // request has fetched all the necessary data,
-      // so we create a snapshot of the page after the
-      // `data-view` attribute exists in the DOM.
-      renderAfterElementExists: '[data-view]'
-    })
-  })
-]
+// const productionPlugins = [
+//   new PrerenderSpaPlugin({
+//     staticDir: path.join(__dirname, 'dist'),
+//     routes: getRoutes(),
+//     postProcess(renderedRoute) {
+//       renderedRoute.html = renderedRoute.html
+//         .replace(/<script (.*?)>/g, '<script $1 defer>')
+//         .replace('id="app"', 'id="app" data-server-rendered="true"')
+//       return renderedRoute
+//     },
+//     renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+//       // We need to inject a value so we're able to
+//       // detect if the page is currently pre-rendered.
+//       inject: {},
+//       // Our view component is rendered after the API
+//       // request has fetched all the necessary data,
+//       // so we create a snapshot of the page after the
+//       // `data-view` attribute exists in the DOM.
+//       renderAfterElementExists: '[data-view]'
+//     })
+//   })
+// ]
 
 module.exports = {
   // publicPath: process.env.NODE_ENV == 'production' ? 'https://cdn1.infrapedia.com/dist/' : '/',
   configureWebpack: config => {
     if (process.env.NODE_ENV == 'production') {
-      config.plugins.push(...productionPlugins)
+      // config.plugins.push(...productionPlugins)
       config.plugins.push(
         new CompressionPlugin({
           compressionOptions: { level: 11 },
           threshold: 1250,
-          minRatio: 0.8,
-          algorithm(input, compressionOptions, callback) {
-            return gzip(input, compressionOptions, callback)
-          }
+          minRatio: 0.8
+          // algorithm(input, compressionOptions, callback) {
+          //   return gzip(input, compressionOptions, callback)
+          // }
         })
       )
     } else {
@@ -64,7 +64,6 @@ module.exports = {
       .options({
         fix: true
       })
-
     // if (process.env.NODE_ENV == 'production') {
     // config.module.rule('gzip-loader').use('gzip-loader')
     // }
