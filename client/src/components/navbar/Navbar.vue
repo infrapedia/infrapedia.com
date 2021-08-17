a41aec4e904a5934539f2ca4eca2c001833ee93d
manAbl <mblanco@jua.com.co>
2021-08-17 11:08:18 -0500

    loading sponsors from a dotenv variable

--- client/src/components/navbar/Navbar.vue
+++ client/src/components/navbar/Navbar.vue
@@ -133,62 +133,20 @@
             </li>
 
             <li
+              v-for="(item, i) in sponsors"
+              :key="i"
               class="inline-block hidden-sm-and-down relative"
               data-no-outline="true"
               role="listitem"
             >
               <div class="list-item" data-no-hover-bg="true">
-                <a :href="sponsors[0].url" target="_blank">
+                <a :href="item.url" target="_blank">
                   <el-image
-                    :src="sponsors[0].src"
+                    :src="item.src"
                     lazy
                     class="w26 image-sponsor"
                     fit="cover"
-                    alt="catchpoint logo"
-                    referrer-policy="strict-origin-when-cross-origin"
-                  />
-                </a>
-              </div>
-            </li>
-
-            <!-- <el-divider direction="vertical" class="m0 hidden-sm-and-down" /> -->
-            <!-- <li
-              class="inline-block hidden-sm-and-down relative"
-              data-no-outline="true"
-              role="listitem"
-            >
-              <div class="list-item" data-no-hover-bg="true">
-                <a
-                  :href="sponsors[2].url"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  onclick="trackOutboundLink('http://bit.ly/3kpe6sB')"
-                >
-                  <el-image
-                    lazy
-                    :src="sponsors[2].src"
-                    fit="center"
-                    class="w26 mt2 image-sponsor"
-                    alt="ipv4 logo"
-                    referrer-policy="strict-origin-when-cross-origin"
-                  />
-                </a>
-              </div>
-            </li> -->
-
-            <li
-              class="inline-block hidden-sm-and-down relative"
-              data-no-outline="true"
-              role="listitem"
-            >
-              <div class="list-item" data-no-hover-bg="true">
-                <a :href="sponsors[1].url" target="_blank">
-                  <el-image
-                    lazy
-                    :src="sponsors[1].src"
-                    fit="center"
-                    class="w26 image-sponsor ipv4"
-                    alt="ipv4 logo"
+                    :alt="item.alt"
                     referrer-policy="strict-origin-when-cross-origin"
                   />
                 </a>
--- client/src/config/navbarSponsors.js
+++ client/src/config/navbarSponsors.js
@@ -1,19 +1,49 @@
-const sponsors = [
-  {
-    url: 'http://bit.ly/3mkhjdR',
-    alt: 'catchpoint advertiser',
-    src: 'https://cdn1.infrapedia.com/assets/img/catchpoint-logo.png'
-  },
-  {
-    url: 'https://www.edgeuno.com?ref=https://www.infrapedia.com',
-    alt: 'edgeuno advertiser',
-    src: 'https://cdn1.infrapedia.com/assets/img/dark-logo.png'
-  }
-  // {
-  //   url: 'https://www.infrapedia.com/',
-  //   alt: 'ad space available',
-  //   src: 'https://storage.googleapis.com/infrapedia_bucket/sponsors/ads.png'
-  // }
-]
+const sponsors = []
+
+const {
+  VUE_APP_SPONSORS_0_src,
+  VUE_APP_SPONSORS_0_url,
+  VUE_APP_SPONSORS_0_alt,
+  VUE_APP_SPONSORS_1_src,
+  VUE_APP_SPONSORS_1_url,
+  VUE_APP_SPONSORS_1_alt
+} = process.env
+
+if (
+  VUE_APP_SPONSORS_0_src &&
+  VUE_APP_SPONSORS_0_url &&
+  VUE_APP_SPONSORS_0_alt
+) {
+  sponsors.push({
+    url: VUE_APP_SPONSORS_0_url,
+    src: VUE_APP_SPONSORS_0_src,
+    alt: VUE_APP_SPONSORS_0_alt
+  })
+}
+
+if (
+  VUE_APP_SPONSORS_1_src &&
+  VUE_APP_SPONSORS_1_url &&
+  VUE_APP_SPONSORS_1_alt
+) {
+  sponsors.push({
+    url: VUE_APP_SPONSORS_1_url,
+    src: VUE_APP_SPONSORS_1_src,
+    alt: VUE_APP_SPONSORS_1_alt
+  })
+}
+
+// [
+//   {
+//     url: 'http://bit.ly/3mkhjdR',
+//     alt: 'catchpoint advertiser',
+//     src: 'https://cdn1.infrapedia.com/assets/img/catchpoint-logo.png'
+//   },
+//   {
+//     url: 'https://www.edgeuno.com?ref=https://www.infrapedia.com',
+//     alt: 'edgeuno advertiser',
+//     src: 'https://cdn1.infrapedia.com/assets/img/dark-logo.png'
+//   }
+// ]
 
 export default sponsors
